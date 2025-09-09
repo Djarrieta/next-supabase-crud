99ed6ad35fc4ee5dce978d77fd05f01f98e3eabb
Dario Arrieta <darrieta@contractor.ea.com>
2025-09-09 17:12:32 -0500

    feat: update DialogContent styles for improved positioning and responsiveness

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -37,7 +37,7 @@ const DialogContent = forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-md translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 rounded-md",
+        "fixed left-1/2 top-1/2 z-50 grid w-full max-w-md -translate-x-1/2 -translate-y-1/2 gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 rounded-md max-h-[90vh] overflow-y-auto",
         className
       )}
       {...props}
